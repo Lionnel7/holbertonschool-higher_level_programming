02b46418bb48656849bfcdc3814ce894dd891416
Lionnel7 <pierrerichan@gmail.com>
2025-05-08 02:38:57 +0200

    Fix: Implemented the print last digit function

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 99:
-        print("{:02d}, ".format(i), end='')
-    elif i == 99:
-        print("{:02d}".format(i), end='\n')
+for i in range(0, 99):
+    print('{:d} = 0x{:x}'.format(i, i))
