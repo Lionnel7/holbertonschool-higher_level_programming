500894ec3ba529ac2562c78d10e2ba4753f583e2
Lionnel7 <pierrerichan@gmail.com>
2025-05-09 16:22:20 +0200

    addition of all arg

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+import sys
+
+def additionner_arguments():
+    """
+    Calcule la somme de tous les arguments passés au script lors de son exécution.
+    Affiche le résultat suivi d'une nouvelle ligne.
+    """
+    somme = 0
+    
+    for arg in sys.argv[1:]:
+        try:
+            nombre = int(arg)  
+            somme += nombre
+        except ValueError:
+
+            pass
+    print(somme)
+
+if __name__ == "__main__":
+    additionner_arguments()
+
