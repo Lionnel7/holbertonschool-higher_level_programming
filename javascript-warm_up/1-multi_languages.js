16f3b9bbe85fe86ae5fc0241172e5460f8eefeab
lionnel7 <pierrerichan@gmail.com>
2025-07-11 16:08:31 +0200

    jjjj

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 // Print the first line: "C is fun"
-console.log("C is fun");
+console.log('C is fun'); // Changed to single quotes
 
 // Print the second line: "Python is cool"
-console.log("Python is cool");
+console.log('Python is cool'); // Changed to single quotes
 
 // Print the third line: "JavaScript is amazing"
-console.log("JavaScript is amazing");
+console.log('JavaScript is amazing'); // Changed to single quotes
