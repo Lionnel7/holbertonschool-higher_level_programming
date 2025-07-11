0a8c4a85c48b0dcf5fb7d3ec0a745a6d9c6e2385
lionnel7 <pierrerichan@gmail.com>
2025-07-11 15:50:05 +0200

    jjjj

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Create a constant variable named myVar with the specified string value.
+const myVar = "JavaScript is amazing";
+
+// Use console.log() to print the value of myVar to the console.
+console.log(myVar);
